20b148c8c55206b13e7f914cd81bdc0c49adde0f
languint <longuint@proton.me>
2025-11-24 22:00:37 -0600

    refactor: various code improvements in board/defs.rs

--- src/board/defs.rs
+++ src/board/defs.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Piece {
     Pawn,
     Knight,
@@ -8,52 +9,42 @@ pub enum Piece {
 }
 
 impl TryFrom<char> for Piece {
-    type Error = String;
+    type Error = &'static str;
+
     fn try_from(value: char) -> Result<Self, Self::Error> {
-        let value = value.to_ascii_lowercase();
+        const MAP: [Option<Piece>; 26] = {
+            let mut m = [None; 26];
+            m['p' as usize - 97] = Some(Piece::Pawn);
+            m['n' as usize - 97] = Some(Piece::Knight);
+            m['b' as usize - 97] = Some(Piece::Bishop);
+            m['r' as usize - 97] = Some(Piece::Rook);
+            m['q' as usize - 97] = Some(Piece::Queen);
+            m['k' as usize - 97] = Some(Piece::King);
+            m
+        };
 
-        match value {
-            'p' => Ok(Piece::Pawn),
-            'n' => Ok(Piece::Knight),
-            'b' => Ok(Piece::Bishop),
-            'r' => Ok(Piece::Rook),
-            'q' => Ok(Piece::Queen),
-            'k' => Ok(Piece::King),
-            _ => Err(format!("'{value}' is not a valid piece!'")),
+        let c = value.to_ascii_lowercase();
+        let idx = (c as u8).wrapping_sub(b'a');
+        if idx < 26 {
+            MAP[idx as usize].ok_or("invalid piece character")
+        } else {
+            Err("invalid piece character")
         }
     }
 }
 
-pub enum Rank {
-    R1,
-    R2,
-    R3,
-    R4,
-    R5,
-    R6,
-    R7,
-    R8,
-}
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[rustfmt::skip]
+pub enum Rank { R1, R2, R3, R4, R5, R6, R7, R8 }
 
-impl<T> From<T> for Rank
-where
-    T: Into<u8>,
-{
-    fn from(value: T) -> Self {
-        match value.into() / 8 {
-            0 => Rank::R1,
-            1 => Rank::R2,
-            2 => Rank::R3,
-            3 => Rank::R4,
-            4 => Rank::R5,
-            5 => Rank::R6,
-            6 => Rank::R7,
-            7 => Rank::R8,
-            _ => unreachable!(),
-        }
+impl From<u8> for Rank {
+    fn from(value: u8) -> Self {
+        unsafe { std::mem::transmute(value / 8) }
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum File {
     A,
     B,
@@ -65,25 +56,13 @@ pub enum File {
     H,
 }
 
-impl<T> From<T> for File
-where
-    T: Into<u8>,
-{
-    fn from(value: T) -> Self {
-        match value.into() % 8 {
-            0 => File::A,
-            1 => File::B,
-            2 => File::C,
-            3 => File::D,
-            4 => File::E,
-            5 => File::F,
-            6 => File::G,
-            7 => File::H,
-            _ => unreachable!(),
-        }
+impl From<u8> for File {
+    fn from(value: u8) -> Self {
+        unsafe { std::mem::transmute(value % 8) }
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[rustfmt::skip]
 #[repr(u8)]
 pub enum Square {
@@ -100,7 +79,7 @@ pub enum Square {
 impl Square {
     pub fn new(file: File, rank: Rank) -> Self {
         let index = (rank as u8) * 8 + (file as u8);
-        unsafe { Square::try_from(index).unwrap_unchecked() }
+        unsafe { std::mem::transmute::<u8, Square>(index) }
     }
 
     pub fn file(self) -> File {
@@ -126,5 +105,57 @@ impl TryFrom<u8> for Square {
         } else {
             Err(format!("'{value}' is not a valid square index"))
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::board::defs::{File, Piece, Rank, Square};
+
+    #[test]
+    fn test_piece_try_from_valid() {
+        assert_eq!(Piece::try_from('p').unwrap(), Piece::Pawn);
+        assert_eq!(Piece::try_from('N').unwrap(), Piece::Knight);
+        assert_eq!(Piece::try_from('b').unwrap(), Piece::Bishop);
+        assert_eq!(Piece::try_from('R').unwrap(), Piece::Rook);
+        assert_eq!(Piece::try_from('q').unwrap(), Piece::Queen);
+        assert_eq!(Piece::try_from('K').unwrap(), Piece::King);
+    }
+
+    #[test]
+    fn test_piece_try_from_invalid() {
+        assert!(Piece::try_from('x').is_err());
+        assert!(Piece::try_from('1').is_err());
+        assert!(Piece::try_from(' ').is_err());
+    }
+
+    #[test]
+    fn test_file_roundtrip() {
+        for i in 0..64 {
+            let file = File::from(i);
+            assert_eq!(file as u8, i % 8);
+        }
+    }
+
+    #[test]
+    fn test_rank_roundtrip() {
+        for i in 0..64 {
+            let rank = Rank::from(i);
+            assert_eq!(rank as u8, i / 8);
+        }
+    }
+
+    #[test]
+    fn test_square_try_from_valid() {
+        for i in 0u8..=63 {
+            let sq = Square::try_from(i).unwrap();
+            assert_eq!(u8::from(sq), i);
+        }
+    }
+
+    #[test]
+    fn test_square_try_from_invalid() {
+        assert!(Square::try_from(64u8).is_err());
+        assert!(Square::try_from(100u8).is_err());
     }
 }
