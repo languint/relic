312d3bcb07c71b6d39c666da21fc2de04cb51cef
languint <longuint@proton.me>
2025-11-24 21:20:00 -0600

    feat: add Piece struct to board/defs.rs

--- src/board/defs.rs
+++ src/board/defs.rs
@@ -1,1 +1,25 @@
+pub enum Piece {
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King,
+}
 
+impl TryFrom<char> for Piece {
+    type Error = String;
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        let value = value.to_ascii_lowercase();
+
+        match value {
+            'p' => Ok(Piece::Pawn),
+            'n' => Ok(Piece::Knight),
+            'b' => Ok(Piece::Bishop),
+            'r' => Ok(Piece::Rook),
+            'q' => Ok(Piece::Queen),
+            'k' => Ok(Piece::King),
+            _ => Err(format!("'{value} is not a valid piece!'")),
+        }
+    }
+}
